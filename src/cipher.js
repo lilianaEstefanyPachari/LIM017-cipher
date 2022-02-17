893ff64aa7c2c11945241f070aa5673f0dd41693
liliana <lilianapachari21@gmail.com>
2022-02-17 10:47:58 -0500

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,61 @@
 const cipher = {
-  // ...
+  encode: function (offsetValue, stringValue) {
+    if(isNaN(offsetValue) == true || typeof offsetValue == "object"){
+      throw new TypeError ("no hay un numero");   
+    }
+    if(offsetValue == "0"){
+      throw new TypeError ("no hay un numero");   
+    }
+    if(typeof stringValue !== "string"){
+     throw new TypeError ("el valor no es una cadena de texto");
+    }
+    let theCipherKey = "";
+    //obteniendo num ascii
+    for (let i = 0; i < stringValue.length; i++) {
+      let getAsciiNumbers = stringValue.charCodeAt(i);
+      //aplicando la formula para el cifrado
+      let startCipher = ((getAsciiNumbers - 65 + offsetValue) % 26) + 65;
+      //obteniendo la letra
+      let newKey = String.fromCharCode(startCipher);
+      theCipherKey += newKey;
+    }
+
+    console.log(theCipherKey);
+    return theCipherKey;
+  },
+
+  decode: function (offsetValue, stringValue) {
+    if(isNaN(offsetValue) == true || typeof offsetValue == "object"){
+      throw new TypeError ("no hay un numero");   
+    }
+    if(offsetValue == "0"){
+      throw new TypeError ("no hay un numero");   
+    }
+    if(typeof stringValue !== "string"){
+     throw new TypeError ("el valor no es una cadena de texto");
+    }
+
+    let theDecodeKey = "";
+    //obteniendo num ascii
+    for (let i = 0; i < stringValue.length; i++) {
+      let getAsciiNumbers = stringValue.charCodeAt(i);
+      //aplicando la formula para el descifrado
+      let startDecode = ((getAsciiNumbers + 65 - offsetValue) % 26) + 65;
+      if (startDecode < 65) {
+        //si la nueva letra es menor que 65 o sea A
+        startDecode += 26;
+      }
+      if (startDecode > 90) {
+        //si la nueva letra es mayor que 90 o sea Z
+        startDecode -= 26;
+      }
+      //obteniendo la letra
+      let originalKey = String.fromCharCode(startDecode);
+      theDecodeKey += originalKey;
+    }
+    console.log(theDecodeKey);
+    return theDecodeKey;
+  },
 };
 
-export default cipher;
+export default cipher; //exportando el objeto cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,81 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
+//cipher.encode();
+//cipher.decode();
 
-console.log(cipher);
+
+const encodePage = document.getElementById("encodePageBtn");
+encodePage.addEventListener("click", showEncodePage);
+function showEncodePage() {
+  document.getElementById("encodePage").style.display = "block";
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("pageMessage").style.display = "block";
+  document.getElementById("pageMessage").innerHTML = "CREA TU CLAVE CIFRADA!";
+}
+
+const decodePage = document.getElementById("decodePageBtn");
+decodePage.addEventListener("click", showDecodePage);
+function showDecodePage() {
+  document.getElementById("decodePage").style.display = "block";
+  document.getElementById("homepage").style.display = "none";
+  document.getElementById("pageMessage").style.display = "block";
+  document.getElementById("pageMessage").innerHTML = "DESCIFRA TU CLAVE!";
+
+}
+
+const toHomepage = document.getElementById("home");
+toHomepage.addEventListener("click", showHomepage);
+function showHomepage() {
+  document.getElementById("homepage").style.display = "block";
+  document.getElementById("encodePage").style.display = "none";
+  document.getElementById("decodePage").style.display = "none";
+  document.getElementById("pageMessage").style.display = "none";
+}
+
+
+//poner en mayuscula todo el texto del input
+document.getElementById("originalKey").onkeyup = function(){myFunctionToUpperCase()};
+
+function myFunctionToUpperCase() {
+  var theInput = document.getElementById("originalKey");
+  theInput.value = theInput.value.toUpperCase();
+}
+//obtener datos del input text y number
+const start = document.getElementById("cipherButton");
+start.addEventListener("click", functionSaveData);
+
+function functionSaveData() {
+  let stringValue = document.getElementById("originalKey").value;
+  let offsetValue = parseInt(document.getElementById("offset").value);
+  if (stringValue == "") {
+    return alert("ingresa la clave que deseas cifrar");
+  }
+  if (isNaN(offsetValue) == true) {
+    return alert("ingresa el desplazamiento para el cifrado");
+  }
+  let stringCipher = cipher.encode(offsetValue, stringValue);
+  document.getElementById("cipherKey").value = stringCipher;
+}
+
+//poner en mayuscula todo el texto del input decode
+document.getElementById("decodeKey").onkeyup = function(){myFunctionToUpperCaseDecode()};
+
+function myFunctionToUpperCaseDecode() {
+  let theInput = document.getElementById("decodeKey");
+  theInput.value = theInput.value.toUpperCase();
+}
+//obtener datos del input text y number
+const startDecode = document.getElementById("decodeBtn");
+startDecode.addEventListener("click", functionSaveDecodeData);
+
+function functionSaveDecodeData() {
+  let stringValue = document.getElementById("decodeKey").value;
+  let offsetValue = parseInt(document.getElementById("offsetDecode").value);
+  if (stringValue == "") {
+    return alert("ingresa la clave que deseas descifrar");
+  }
+  if (isNaN(offsetValue) == true) {
+    return alert("ingresa el desplazamiento para el descifrado");
+  }
+  let stringDecode = cipher.decode(offsetValue, stringValue);
+  document.getElementById("myDecodeKey").value = stringDecode;
+}
